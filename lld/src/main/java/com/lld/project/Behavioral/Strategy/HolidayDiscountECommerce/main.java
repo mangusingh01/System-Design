2efadd1278b3d06a3887ecc366770ffce55d9d2f
Mangu Singh <mangusingh@Mangus-MacBook-Pro.local>
2026-01-31 01:45:15 +0530

    project created

---
+++ lld/src/main/java/com/lld/project/Behavioral/Strategy/HolidayDiscountECommerce/main.java
@@ -0,0 +1,7 @@
+package com.lld.project.Behavioral.Strategy.HolidayDiscountECommerce;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("this is HolidayDiscount.....");
+    }
+}
---
+++ lld/src/main/java/com/lld/project/Behavioral/Strategy/NearestDriverStrategy/main.java
@@ -0,0 +1,7 @@
+package com.lld.project.Behavioral.Strategy.NearestDriverStrategy;
+
+public class main {
+    public static void main(String[] args) {
+
+    }
+}
